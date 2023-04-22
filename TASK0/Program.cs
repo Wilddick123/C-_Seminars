cc7813da0aea109ae48078ef68d1089cab1e0999
Ivan Isakov <wilcs702@gmail.com>
2023-04-22 12:38:22 +0300

    Add TASK0 and TASK1

---
+++ TASK0/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+
+// Например:
+// 4 -> 16 
+// -3 -> 9 
+// -7 -> 49
+
+Console.Write("Введите число: ");
+int inNumber = Convert.ToInt32(Console.ReadLine());
+int sqNumber = inNumber * inNumber;
+Console.Write($"Квадрат числа {inNumber} = {sqNumber}");
+ //Console.WriteLine(sqNumber);
---
+++ TASK1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+// a = 25, b = 5 -> да 
+// a = 2, b = 10 -> нет 
+// a = 9, b = -3 -> да 
+// a = -3 b = 9 -> нет
+// 3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 	3 -> Среда 
+// 5 -> Пятница
+
+
+Console.Write("Введите первое число: ");
+int inNumberOne = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int inNumberTwo = Convert.ToInt32(Console.ReadLine());
+
+int sqNumber = inNumberTwo * inNumberTwo;
+
+if(inNumberOne == sqNumber){
+    Console.Write($"Число {inNumberOne} является квадратом числа {inNumberTwo}");
+}
+else{
+    Console.Write($"Число {inNumberOne} не является квадратом числа {inNumberTwo}");
+}
